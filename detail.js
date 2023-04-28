33ba125f62c9edc72f187f5190c76c185ac7bc2f
JingWang12345 <127298095+JingWang12345@users.noreply.github.com>
2023-04-28 09:56:23 +0200

    type

--- detail.js
+++ detail.js
@@ -13,25 +13,39 @@ console.log(pokemonName);
 
 PokeService.getDetail(pokemonName).then(pokemonObject => {
     console.log('data from db', pokemonObject);
-    const myPokemon = new Pokemon(pokemonObject.name, pokemonObject.stats, pokemonObject.sprites.front_default);
-    console.log('pokemon', myPokemon);
+
+    const newPokemon = createNewPokemon(pokemonObject);
+   
+    displayPokemon(newPokemon);
+})
+
+function displayPokemon(pokemon){
+document.getElementById('pokemon-name').innerHTML = pokemon.name;
+
+const statsContainer = document.getElementById('pokemon-stats');
+for (const stat of pokemon.stats) {
+    statsContainer.innerHTML +=`<li><strong>${stat.name}</strong>${stat.baseValue}</li>`
+}
+
+const typeContainer = document.getElementById('pokemon-types');
+for (const type of pokemon.types) {
+    typeContainer.innerHTML +=`<li><strong>${type.name}</strong>${type.url}</li>`
+}
+     
+}
+
+function createNewPokemon(pokemonObject) {
+    const myPokemon = new Pokemon(pokemonObject.name);
+
     for (let i = 0; i < pokemonObject.stats.length; i++) {
         const statObject = pokemonObject.stats[i];
         myPokemon.addStat(statObject.stat.name, statObject.base_stat)
     }
-    console.log('pokemon', myPokemon);
-    displayPokemon(myPokemon);
-})
 
-function displayPokemon(pokemon){
-     const pokemonDetail = document.getElementById('pokemon-detail');
-     const nameContainer = document.createElement('span');
-     const nameText = document.createTextNode(pokemon.name);
-     nameContainer.appendChild(nameText);
-     pokemonDetail.appendChild(nameContainer);
+    for (let i = 0; i < pokemonObject.types.length; i++) {
+        const typeObj = pokemonObject.types[i];
+        myPokemon.addType(typeObj.type.name, typeObj.type.url);
+    }
 
-     const imageContainer = document.createElement('img');
-     imageContainer.src=pokemon.image;
-     pokemonDetail.appendChild(imageContainer);
-     
+    return myPokemon;
 }
